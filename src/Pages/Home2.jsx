1e36c176e69e507fe086d3c0839c10e6a6cf279a
ashrx19 <ashwin191205@gmail.com>
2026-09-06 15:21:05 +0530

    RECIPIE

--- src/Pages/Home2.jsx
+++ src/Pages/Home2.jsx
@@ -19,6 +19,9 @@ function Home2(){
     <h1>Home</h1>
 
 
+
+
+
     <input type="text" placeholder={search} onChange={(e)=>{setSearch(e.target.value) }}/>
     <>
         {recipies.map((r)=>(<h1>{r.strMeal}</h1>))}
