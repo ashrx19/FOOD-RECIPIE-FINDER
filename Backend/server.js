be3f915f5478d9c406b353824e1440eb3876ce11
ashrx19 <ashwin191205@gmail.com>
2026-09-12 16:42:10 +0530

    new commit

---
+++ Backend/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+
+
+
+const app = express();
+const PORT =5000;
+
+app.use(express.json())
+
+app.get("/api/recipes",(req,res)=>{
+    res.json({
+        "message": "Recipes fetched successfully",
+        "recipes": []
+    })
+})
+
+
+app.listen(PORT);
