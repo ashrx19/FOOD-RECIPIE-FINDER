c7ec1fe6b1981f947befae7adae7e290f01d13e6
ashrx19 <ashwin191205@gmail.com>
2026-09-23 16:43:33 +0530

    FAV ICON

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Heart } from "lucide-react";
 
 function RecipeDetails() {
 
@@ -7,6 +8,8 @@ function RecipeDetails() {
     console.log(id);
 
     const[recipe,setRecipe]=useState(null);
+    const [fav, setFav] = useState(false);
+
     
     useEffect(() => {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)    .then((res) => res.json())
@@ -17,16 +20,46 @@ function RecipeDetails() {
 
     if (!recipe) return <h2>Not found</h2>;
 
+    const handleFav = async () => {
+
+      try {
+        if (!fav) {
+
+          // Add favorite
+          await axios.post(
+            `/api/recipes/fav/${recipe.idMeal}`
+          );
+          setFav(true);
+
+        } else {
+
+          // Remove favorite
+          await axios.delete(
+            `/api/recipes/fav/${recipe.idMeal}`
+          );
+          setFav(false);
+        }
+
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     
   return (
     <div className="RecipeDetails">
       <h1>{recipe.strMeal}</h1>
+
+      <button className="fav-button" onClick={handleFav}>
+        <Heart size={24} fill={fav ? "red" : "none"} color={fav ? "red" : "white"} />
+      </button>
 
       <img
         src={recipe.strMealThumb}
         alt={recipe.strMeal}
         width="400"
       />
+
 
       <h2>Category</h2>
       <p>{recipe.strCategory}</p>
